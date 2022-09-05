f2d3c63f32b99a1ab85c5dc9035d0d84fafe3911
Dmitry Sharabin <dmitrysharabin@gmail.com>
2022-09-05 15:46:37 +0200

    Temperature Converter

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,17 @@
+<!-- https://eugenkiss.github.io/7guis/tasks#temp -->
 <script>
+  let c, f;
 
+  function toFahrenheit () {
+    f = c * (9/5) + 32;
+  }
+
+  function toCelsius () {
+    c = (f - 32) * (5/9)
+  }
 </script>
 
 <main>
-
+  <input type="number" bind:value={c} on:input={toFahrenheit} /> Celsius =
+  <input type="number" bind:value={f} on:input={toCelsius} /> Fahrenheit
 </main>
-
-<style>
-
-</style>
