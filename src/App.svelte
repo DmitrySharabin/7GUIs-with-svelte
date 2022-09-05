17f557bdb7b3f09c94a58ee76f4fd261f384839c
Dmitry Sharabin <dmitrysharabin@gmail.com>
2022-09-05 16:27:25 +0200

    Flight Booker

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,46 @@
-<!-- https://eugenkiss.github.io/7guis/tasks#temp -->
+<!-- https://eugenkiss.github.io/7guis/tasks#flight -->
 <script>
-  let c, f;
+	let flight;
+	let departureDate = getISODate(new Date());
+	let returnDate = getISODate(new Date());
 
-  function toFahrenheit () {
-    f = c * (9/5) + 32;
-  }
+	$: canBook = flight === "one way" || Date.parse(returnDate) > Date.parse(departureDate);
 
-  function toCelsius () {
-    c = (f - 32) * (5/9)
-  }
+	const showConfirmation = () => {
+		const message =
+			flight === "return"
+				? `You have booked a return flight leaving on ${departureDate} and returning on ${returnDate}.`
+				: `You have booked a one-way flight leaving on ${departureDate}.`;
+
+		alert(message);
+	};
+
+	function getISODate(date) {
+		return date.toISOString().split("T")[0];
+	}
 </script>
 
 <main>
-  <input type="number" bind:value={c} on:input={toFahrenheit} /> Celsius =
-  <input type="number" bind:value={f} on:input={toCelsius} /> Fahrenheit
+	<select bind:value={flight}>
+		<option value="one way">One-way Flight</option>
+		<option value="return">Return Flight</option>
+	</select>
+
+	<input type="date" bind:value={departureDate} />
+
+	<input type="date" bind:value={returnDate} disabled={flight !== "return"} />
+
+	<button disabled={!canBook} on:click={showConfirmation}>Book</button>
+
+	{#if !canBook}
+		<p>⚠️ Return date must be after departure date!</p>
+	{/if}
 </main>
+
+<style>
+	main {
+		display: grid;
+		grid-template-columns: min-content;
+		gap: 0.3em;
+	}
+</style>
