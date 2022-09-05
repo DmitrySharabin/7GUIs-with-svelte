f4a340d13487dabbd56ccff34887684da8086716
Dmitry Sharabin <dmitrysharabin@gmail.com>
2022-09-05 15:46:37 +0200

    Counter

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,9 @@
+<!-- https://eugenkiss.github.io/7guis/tasks#counter -->
 <script>
-
+  let counter = 0;
 </script>
 
 <main>
-
+  <input readonly value={counter} />
+  <button on:click={() => counter += 1}>Count</button>
 </main>
-
-<style>
-
-</style>
